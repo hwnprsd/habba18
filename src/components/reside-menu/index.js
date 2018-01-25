541a65e94e1c3fad6b619a3d002e17796016a6d2
Ashwin Prasad <d3fkonmusic@gmail.com>
2018-01-25 14:36:20 +0530

    added menu options for reside menu

---
+++ src/components/reside-menu/index.js
@@ -0,0 +1,176 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Button,
+    Animated,
+    PanResponder,
+    TouchableOpacity
+} from 'react-native';
+import { fonts, height, width } from '../../constants';
+import { observer, inject } from 'mobx-react/native';
+import ElevatedView from 'react-native-elevated-view'
+
+import styles from './styles';
+
+let navigate
+
+
+const helper = (x, v) => {
+    if (v > 0) {
+        if (x > 300)
+            return 300;
+        if (x < 0)
+            return 0;
+        if (x > 0)
+            return 300;
+    }
+    if (v < 0) {
+        if (x < -300)
+            return -300;
+        if (x < 0)
+            return -300;
+        if (x > 0)
+            return 0
+    }
+    if (v === 0) {
+        if (x < -300)
+            return -300;
+        if (x > 300)
+            return 300;
+    }
+    return 0;
+}
+
+const AnimatedElevatedView = Animated.createAnimatedComponent(ElevatedView);
+
+const Item = (props) => {
+    const { text, right, nav } = props;
+    return (
+        <TouchableOpacity style={styles.itemContainer}>
+            <Text
+                style={[styles.itemText, { textAlign: right ? 'right' : 'left' }]}
+                onPress={nav && (() => navigate(nav))}
+            >{text}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+
+const List = () => {
+    return (
+        <View style={{ flex: 2, flexDirection: 'row' }}>
+            <View style={{ flex: 3 }}>
+                <View style={{ flex: 1 }}></View>
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <View style={{ justifyContent: 'space-around', paddingLeft: 10 }}>
+                        <Item text="Register" />
+                        <Item text="Events" nav="CategoryList" />
+                        <Item text="Timeline" nav="Timeline" />
+                        <Item text="Maps" />
+                        <Item text="Devs" />
+                    </View>
+                </View>
+                <View style={{ flex: 1 }}></View>
+            </View>
+            <View style={{ flex: 3 }}>
+                <View style={{ flex: 1 }}></View>
+                <View style={{ flex: 1, alignItems: 'flex-end', justifyContent: 'center' }}>
+                    <View style={{ justifyContent: 'space-around', paddingRight: 10 }}>
+                        <Item text="Memes" right nav="Meme" />
+                        <Item text="Notifications" right />
+                        <Item text="QR Code" right />
+                        <Item text="About us" right />
+                        <Item text="Logout" right />
+                    </View>
+
+                </View>
+                <View style={{ flex: 1 }}></View>
+            </View>
+
+        </View>
+    )
+}
+
+export default class ResideMenu extends Component {
+    state = {
+        animatedValueX: 0,
+        animatedVelocity: 0
+    }
+    render() {
+        const style = { width, height, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }
+        // const animatedStyle = { transform: this.animatedValue.getTranslateTransform() }
+        const animatedStyle = {
+            transform: [
+                {
+                    translateX: this.animatedValue.x.interpolate({
+                        inputRange: [-200, -50, 0, 50, 200],
+                        outputRange: [-60, 0, 0, 0, 60]
+                    })
+                },
+                {
+                    scale: this.animatedValue.x.interpolate({
+                        inputRange: [-200, -50, 0, 50, 200],
+                        outputRange: [0.8, 1, 1, 1, 0.8]
+                    })
+                }
+            ]
+        }
+
+        return (
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={{ flex: 1, backgroundColor: '#e2e1e0' }}>
+                    <List />
+                </View>
+                <AnimatedElevatedView
+                    elevation={7}
+                    style={[animatedStyle, style, { position: 'absolute' }]}
+                    {...this._panResponder.panHandlers}
+                >
+                    <Text>AHASHASH</Text>
+                </AnimatedElevatedView>
+            </View>
+        )
+    }
+    componentWillMount = () => {
+        navigate = this.props.navigation.navigate
+        this.animatedValue = new Animated.ValueXY();
+        this.animatedValue.x.addListener(v => { this.state.animatedValueX = v.value });
+        this._panResponder = PanResponder.create({
+            // Ask to be the responder:
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+            onPanResponderGrant: (evt, gestureState) => {
+                this.animatedValue.setOffset({ x: this.state.animatedValueX });
+                this.animatedValue.setValue({ x: 0, y: 0 }); //Initial value
+            },
+            onPanResponderMove: Animated.event([
+                null,
+                { dx: this.animatedValue.x, vx: this.state.animatedVelocity }
+            ]),
+            onPanResponderTerminationRequest: (evt, gestureState) => true,
+            onPanResponderRelease: (evt, gestureState) => {
+                Animated.parallel([
+                    Animated.spring(this.animatedValue.x, {
+                        toValue: helper(parseInt(this.state.animatedValueX), parseFloat(gestureState.vx)),
+                        useNativeDriver: true
+                    })
+                ]).start()
+                this.animatedValue.flattenOffset();
+
+
+            },
+            onPanResponderTerminate: (evt, gestureState) => {
+            },
+            onShouldBlockNativeResponder: (evt, gestureState) => {
+                return true;
+            },
+        });
+    };
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import {
-    View,
-    Text,
-    Button,
-    Animated,
-    PanResponder
-} from 'react-native';
-import { fonts } from './constants';
-import { observer, inject } from 'mobx-react/native';
-import { SkypeIndicator } from 'react-native-indicators';
-import Auth from './components/auth';
 import CategoryList from './components/category-list';
 import EventList from './components/event-list';
 import Timeline from './components/timeline';
-import Meme from './components/meme'
+import Meme from './components/meme';
+import ResideMenu from './components/reside-menu';
 import { StackNavigator } from 'react-navigation';
 
-import { height, width } from './constants'
 const MainStack = StackNavigator({
+    ResideMenu: { screen: ResideMenu },
     Timeline: { screen: Timeline },
     CategoryList: { screen: CategoryList },
     Meme: { screen: Meme },
@@ -26,109 +16,8 @@ const MainStack = StackNavigator({
         headerMode: 'none'
     })
 
-
-const helper = (x, v) => {
-    if (v > 0) {
-        if (x > 300)
-            return 300;
-        if (x < 0)
-            return 0;
-        if (x > 0)
-            return 300;
-    }
-    if (v < 0) {
-        if (x < -300)
-            return -300;
-        if (x < 0)
-            return -300;
-        if (x > 0)
-            return 0
-    }
-    if (v === 0) {
-        if (x < -300)
-            return -300;
-        if (x > 300)
-            return 300;
-    }
-    return 0;
-}
-
-
-@inject('authStore') @observer
 export default class App extends Component {
-    state = {
-        animatedValueX: 0,
-        animatedVelocity: 0
-    }
     render() {
-        const style = { position: 'absolute', width, height, justifyContent: 'flex-start', zIndex: 5, backgroundColor: 'blue', alignItems: 'center', justifyContent: 'center' }
-        // const animatedStyle = { transform: this.animatedValue.getTranslateTransform() }
-        const animatedStyle = {
-            transform: [
-                {
-                    translateX: this.animatedValue.x.interpolate({
-                        inputRange: [-200, -50, 0, 50, 200],
-                        outputRange: [-60, 0, 0, 0, 60]
-                    })
-                },
-                {
-                    scale: this.animatedValue.x.interpolate({
-                        inputRange: [-200, -50, 0, 50, 200],
-                        outputRange: [0.8, 1, 1, 1, 0.8]
-                    })
-                }
-            ]
-        }
-        return (
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-                <View style={{ flex: 1, backgroundColor: 'red' }}>
-                </View>
-
-                <Animated.View
-                    style={[style, animatedStyle]}
-                    {...this._panResponder.panHandlers}
-                >
-                </Animated.View>
-            </View>
-        )
+        return <MainStack />
     }
-    componentWillMount = () => {
-        this.animatedValue = new Animated.ValueXY();
-        this.animatedValue.x.addListener(v => { this.state.animatedValueX = v.value });
-        this._panResponder = PanResponder.create({
-            // Ask to be the responder:
-            onStartShouldSetPanResponder: (evt, gestureState) => true,
-            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-            onMoveShouldSetPanResponder: (evt, gestureState) => true,
-            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-
-            onPanResponderGrant: (evt, gestureState) => {
-                this.animatedValue.setOffset({ x: this.state.animatedValueX });
-                this.animatedValue.setValue({ x: 0, y: 0 }); //Initial value
-            },
-            onPanResponderMove: Animated.event([
-                null,
-                { dx: this.animatedValue.x, vx: this.state.animatedVelocity }
-            ]),
-            onPanResponderTerminationRequest: (evt, gestureState) => true,
-            onPanResponderRelease: (evt, gestureState) => {
-                Animated.parallel([
-                    Animated.spring(this.animatedValue.x, {
-                        toValue: helper(parseInt(this.state.animatedValueX), parseFloat(gestureState.vx)),
-                        useNativeDriver: true
-                    })
-                ]).start()
-                this.animatedValue.flattenOffset();
-
-
-            },
-            onPanResponderTerminate: (evt, gestureState) => {
-            },
-            onShouldBlockNativeResponder: (evt, gestureState) => {
-                return true;
-            },
-        });
-    };
-
-
 }
