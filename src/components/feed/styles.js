ec29d29d1e4ca838a2a4835d0611f52f51f95255
Ashwin Prasad <d3fkonmusic@gmail.com>
2018-01-26 20:34:36 +0530

    added youtube support for feeds

---
+++ src/components/feed/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { width } from '../../constants';
+
+export default StyleSheet.create({
+    postContainer: {
+        flex: 1,
+        paddingTop: 5,
+        backgroundColor: 'white',
+        marginBottom: 10
+    },
+    headingText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 5
+    },
+    captionText: {
+        fontSize: 15,
+        marginLeft: 5
+    }
+})
