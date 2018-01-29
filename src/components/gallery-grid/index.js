3d893eecf6598f78009ef7f24c1c0a1a22b34b07
Ashwin Prasad <d3fkonmusic@gmail.com>
2018-01-29 17:24:46 +0530

    cleaned up gallery packages

--- src/components/gallery-grid/index.js
+++ src/components/gallery-grid/index.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import PopupDialog from 'react-native-popup-dialog';
-import GridView from 'react-native-super-grid';
-import FastImage from 'react-native-fast-image';
-import Header from '../header';
-import { width, height } from '../../constants';
+import { View } from 'react-native';
 import { observer, inject } from 'mobx-react/native';
 import PhotoBrowser from 'react-native-photo-browser';
-import Gallery from '../gallery';
 
 import styles from './styles';
 
@@ -16,15 +10,15 @@ export default class GalleryGrid extends Component {
     render() {
         const { allImages } = this.props.galleryStore;
         return (
-            <View style={{flex: 1}}>
-        
+            <View style={{ flex: 1 }}>
+
                 <PhotoBrowser
                     mediaList={allImages}
                     startOnGrid
                     enableGrid
                     square
                     displayNavArrows
-                    onBack={() => {this.props.navigation.goBack()}}
+                    onBack={() => { this.props.navigation.goBack() }}
                 />
             </View>
         )
